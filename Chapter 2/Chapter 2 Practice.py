eb6c8f2aae075f8e5e227ec49e40337ca4650cf7
gageb438 <gageb534@gmail.com>
2024-08-20 13:50:01 -0500

    Update Chapter 2 Practice.py

--- Chapter 2/Chapter 2 Practice.py
+++ Chapter 2/Chapter 2 Practice.py
@@ -10,5 +10,18 @@ def practice2():
     # it outputs another message to the user
     print("This is practice 2.")
 
-
 # hit run to use the command bar
+
+def comment_example():
+    # comment_example recieves no arguments
+    # it explains how to create a function header
+    # and  outputs or returns "Hello World!".
+    print("Hello World!")
+    
+def program2_1():
+    # program2_1 recieves no arguments
+    # it outputs 3 comments in one function.
+    print ('Kate Austen')
+    print ('123 Full Circle Drive')
+    print ('Asheville, NC 28899')
+    
