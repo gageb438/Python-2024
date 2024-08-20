152c7aaa70c86a2bb5643d7c896256fabc119ef0
gageb438 <gageb534@gmail.com>
2024-08-20 13:40:28 -0500

    Update Chapter 2 Practice.py

--- Chapter 2/Chapter 2 Practice.py
+++ Chapter 2/Chapter 2 Practice.py
@@ -1,1 +1,14 @@
 # Chapter 2 Practice
+
+def practice1():
+    # practice 1 recieves no arguments
+    # it outputs a message to the user
+    print("This is practice 1.")
+    
+def practice2():
+    # practice 2 recieves no arguments
+    # it outputs another message to the user
+    print("This is practice 2.")
+
+
+# hit run to use the command bar
