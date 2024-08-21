e5158c64b334512968ea45f921fe3c1b74a68eb8
gageb438 <gageb534@gmail.com>
2024-08-21 13:31:45 -0500

    Update Chapter 2 Practice.py

--- Chapter 2/Chapter 2 Practice.py
+++ Chapter 2/Chapter 2 Practice.py
@@ -1,25 +1,25 @@
 # Chapter 2 Practice
 
 def practice1():
-    # practice 1 recieves no arguments
+    # practice 1 accepts no arguments
     # it outputs a message to the user
     print("This is practice 1.")
     
 def practice2():
-    # practice 2 recieves no arguments
+    # practice 2 accepts no arguments
     # it outputs another message to the user
     print("This is practice 2.")
 
 # hit run to use the command bar
 
 def comment_example():
-    # comment_example recieves no arguments
+    # comment_example accepts no arguments
     # it explains how to create a function header
     # and  outputs or returns "Hello World!".
     print("Hello World!")
     
 def program2_1():
-    # program2_1 recieves no arguments
+    # program2_1 accepts no arguments
     # apostrophe output
     # it outputs 3 comments in one function.
     print ('Kate Austen')
@@ -27,7 +27,7 @@ def program2_1():
     print ('Asheville, NC 28899')
     
 def program2_2():
-    # program2_2 recieves no arguments.
+    # program2_2 accepts no arguments.
     # double apostrophe output.
     # it outputs 3 comments in one function.
     print ("Kate Austen")
@@ -35,5 +35,45 @@ def program2_2():
     print ("Asheville, NC 28899")
     
 def program2_3():
+    # program 2_3 accepts no arguments.
+    # outputs 2 comments in one function.
+    print ("Don't fear!")
+    print ("I'm here!")
+
+def program2_4():
+    # program 2_4 accepts no arguments
+    # use backslash before the start and before the end quotation to ignore characters.
+    print ("I read \"Hamlet\" this summer.")
+    
+def program2_7():
+    # program 2_7 accepts no arguments
+    # it uses the variable room to store the value 503
+    # and prints the message to the user
+    
+    # assigns 503 to RoomNumber
+    RoomNumber = 503
     
     
+    # outputs the message to the user
+    print ("I am staying in room number")
+    print (RoomNumber)
+    
+    
+    # outputs the message to the user using a second way
+    print ("I am staying in room number", RoomNumber)
+
+def program2_8():
+    # program2_8 accepts no arguments
+    
+    # assigns numbers to the variables
+    TopSpeed = 160
+    Distance = 300
+    
+    # outputs the message to the user
+    print ("The top speed is")
+    print (TopSpeed)
+    
+    # outputs the message to the user
+    print ("The distance traveled is")
+    print (Distance)
+
