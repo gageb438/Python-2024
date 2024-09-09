86b21effb6a18949f5cefec1e908ab0a67b13d99
gageb438 <gageb534@gmail.com>
2024-09-09 13:47:48 -0500

    Update Chapter 3 Practice.py

--- Chapter 3/Chapter 3 Practice.py
+++ Chapter 3/Chapter 3 Practice.py
@@ -25,6 +25,14 @@ def test_average():
     firstScore = float(input("Enter the first score: "))
     secondScore = float(input("Enter the second score: "))
     thirdScore = float(input("Enter the third score: "))
-    
+    highScore = 95.00
+    # finds the average score
     averageScore = ((firstScore + secondScore + thirdScore) / 3)
     
+    # prints the average score
+    print("The average score is: %", format(averageScore, '.2f'), sep = '')
+    
+    # if the average score is greater than or equal to 95.00 it prints "Congratulations!" "Thats a high score!"
+    if averageScore > highScore:
+        print("Congratulations!")
+        print("Thats a high score!")
