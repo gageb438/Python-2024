e2c0321d388cfb743c8533e4b13c0a35ede97f6b
gageb438 <gageb534@gmail.com>
2024-10-25 13:50:47 -0500

    r

---
+++ Chapter 5/my_graphics.py
@@ -0,0 +1,37 @@
+# imports
+import turtle
+
+def circle(x, y, radius, color):
+    # circle accepts an argument for x, y, the radius, and the color
+    # it draws a circle using those aguments
+    turtle.penup()
+    turtle.goto(x, y - radius)
+    turtle.fillcolor(color)
+    turtle.pendown()
+    turtle.begin_fill()
+    turtle.circle(radius)
+    turtle.end_fill()
+    
+    
+def square(x, y, width, color):
+    # square accepts an argument for x, y, the width, and the color
+    # it draws a square using those aguments
+    turtle.penup()
+    turtle.goto(x, y)
+    turtle.fillcolor(color)
+    turtle.pendown()
+    turtle.begin_fill()
+    for count in range(4):
+        turtle.forward(width)
+        turtle.left(90)
+    turtle.end_fill()
+    
+
+def line(startX, startY, endX, endY, color):
+    # line accepts arguments for startx, starty, endx, endy, and color
+    # it draws a line using these arguments
+    turtle.penup()
+    turtle.goto(startX, startY)
+    turtle.pendown()
+    turtle.pencolor(color)
+    turtle.goto(endX, endY)
