06eb6f11ae96acff999318d5f76937c4391caef8
gageb438 <gageb534@gmail.com>
2024-10-29 08:20:50 -0500

    Update demo.py

--- Chapter 5/demo.py
+++ Chapter 5/demo.py
@@ -10,14 +10,15 @@ def main():
     # take input form the user for the radius
     radius = int(input("Enter the radius: "))
     
-    # use the circle.py module to find the area of the circle
+    # use the circlemath.py module to find the area of the circle
     print(f"The area of the circle is: {circlemath.area(radius)}.")
     print(f"The circumference of the circle is: {circlemath.circumference(radius)}.")
     
     # take input from the user for the length and width of a rectangle
     length = int(input("Enter the length of a side: "))
     width = int(input("Enter the width of a side: "))
-    
+
+    # use the rectanglemath.py module to find the area and perimeter of a rectangle
     print(f"The perimeter of the rectangle is: {rectanglemath.perimeter(width, length)}.")
     print(f"The area of the rectangle is: {rectanglemath.area(width, length)}.")
 main()
