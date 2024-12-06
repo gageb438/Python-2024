53e2ca2c824ad25d53f5854dc9f87e687c0d232a
gageb438 <gageb534@gmail.com>
2024-12-06 13:47:29 -0600

    gg

---
+++ Chapter 6/menu.py
@@ -0,0 +1,31 @@
+# imports
+import turtle as t
+
+#==========================
+
+def main(exercise):
+    # main accepts an argument for how many exercises are wanted
+    # it will prompt the user with a menu
+    # and ask the user to make a choice
+    # and will call the function depending on the user choice.
+    
+    # declare variable for the user choice
+    choice = 0
+    
+    # output the menu
+    print()
+    print("Choose a program from the following options.")
+    for exerciseNumber in range(exercise + 1):
+        print(f"{exerciseNumber}: Exercise {exerciseNumber}")
+    
+    # get input for the user choice
+    choice = int(input(":> "))
+    while choice < 1 or choice > exercise:
+        choice = int(input(":> "))
+    print()
+        
+    print(f"Calling... Exercise {choice}...")
+    print()
+    return choice
+
+#==========================
