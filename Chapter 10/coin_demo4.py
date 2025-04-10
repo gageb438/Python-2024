31c9b488832d205aafb028bd0ae63ee9445b3750
Gage <gageb534@gmail.com>
2025-04-10 11:35:26 -0500

    Update coin_demo4.py

--- Chapter 10/coin_demo4.py
+++ Chapter 10/coin_demo4.py
@@ -4,3 +4,25 @@ import coin
 
 def main(): #program 10-6
     # main accepts no arguments
+    # it creates an object my_coin using the coin class in coin.py
+    # it uses the get_sideup() method in the coin class to display the starting state
+    # it loops 10 times, tossing the coin with the toss() method
+    # and displaying the side again with the get_sideup() method
+
+    # initialize the coin and the counter
+    my_coin = coin.Coin()
+    counter = 0
+
+    # print the side up
+    print(f"This side is up: {my_coin.get_sideup()")
+
+    # loop for while the counter isnt 10
+    while counter != 10:
+        counter += 1
+
+        # toss the coin
+        my_coin.toss()
+
+        # print the side that is up
+        print(f"Toss {counter}: {my_coin.get_sideup()}")
+main()
