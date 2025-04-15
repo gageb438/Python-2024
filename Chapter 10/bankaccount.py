c5e87896958df40b0ce89a512e6c0bed6dcda4b1
Gage <gageb534@gmail.com>
2025-04-15 11:28:21 -0500

    commit from VS

---
+++ Chapter 10/bankaccount.py
@@ -0,0 +1,28 @@
+# the bank account class simulates a bank account
+
+class BankAccount(): #10-7
+
+    # the __init__ method accepts an argument for the account's balance.
+    # it is assigned to the __ balance attribute
+
+    def __init__(self, bal):
+        self.__balance = bal
+    
+    # the deposit method makes a deposit into the account
+
+    def deposit(self, amount):
+        self.__balance += amount
+    
+    # the withdraw method makes a withdrawl from the account
+
+    def withdraw(self, amount):
+        if self.__balance > amount:
+            self.__balance -= amount
+        else:
+            print("Error: Insufficenet funds")
+    
+    # the get balance method returns the account balance
+
+    def get_balance(self):
+        return self.__balance
+
