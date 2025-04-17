705e55dc067fa68df2ed5d754d649fb550bfd235
Gage <gageb534@gmail.com>
2025-04-17 11:37:32 -0500

    fin

--- Game/game_classes.py
+++ Game/game_classes.py
@@ -1,69 +1,45 @@
-# all classes for the game
-
 class Hero():
-    def __init__(self, name, health, health_max, mana, mana_max, power_xp, power_xp_max, inventory):
-        self.__name = name
-        self.__health = health
-        self.__health_max = health_max
-        self.__mana = mana
-        self.__mana_max = mana_max
-        self.__power_xp = power_xp
-        self.__power_xp_max = power_xp_max
-        self.__inventory = inventory
+    def __init__(self, health, health_max, weapon):
+        self.__hp = health
+        self.__max_hp = health_max
+        self.__weapon = weapon
+
+    # getters
+    def get_hp(self):
+        return self.__hp
     
-    #--getters--#
-    def get_name(self):
-        return self.__name
+    def get_max_hp(self):
+        return self.__max_hp
+
+    def get_weapon(self):
+        return self.__weapon
     
-    def get_health(self):
-        return self.__health
-    
-    def get_max_health(self):
-        return self.__health_max
-    
-    def get_mana(self):
-        return self.__mana
-    
-    def get_max_mana(self):
-        return self.__mana_max
-    
-    def get_power_xp(self):
-        return self.__power_xp
-    
-    def get_max_power_xp(self):
-        return self.__max_power_xp
-    
-    def get_inventory(self):
-        return self.__inventory
-    
-    #--modifiers--#
-    
-    # healths
-    def lose_health(self, loss):
-        if self.__health <= loss:
-            return "Dead"
+    # modifiers
+
+    # health
+    def gain_hp(self, gain):
+        if gain >= self.__max_hp - self.__hp:
+            self.__hp = self.__max_hp
         else:
-            self.__health -= loss
-            
-    def gain_health(self, gain):
-        if self.__health + gain > self.__health_max:
-            self.__health = self.__health_max
-        else:
-            self.__health += gain
-            
-    # manas
-    def lose_mana(self, loss):
-        if self.__mana <= loss:
-            self.__mana = 0
-        else:
-            self.__mana -= loss
-            
-    def gain_mana(self, gain):
-        if self.__mana + gain >= self.__mana_max:
-            self.__mana = self.__mana_max
-        else:
-            self.__mana += gain
+            self.__hp += gain
     
+    def lose_hp(self, loss):
+        if loss >= self.__hp:
+            self.__hp = 0
+            return False
+        else:
+            self.__hp -= loss
+            return True
+    
+    def s_max_hp(self, max):
+        self.__max_hp = max
+    
+    # weapon
+    def s_weapon(self, weapon):
+        self.__weapon = weapon
+    
+
+
 class Enemy():
     pass
 
---
+++ Game/game_main.py
@@ -0,0 +1,20 @@
+import game_classes
+
+player = game_classes.Hero(100, 100, "Axe")
+
+player.lose_hp(10)
+print(player.get_hp())
+player.gain_hp(10)
+print(player.get_hp())
+
+living = player.lose_hp(99)
+
+print(player.get_hp())
+if living == True:
+    print("Living")
+else:
+    print("Dead")
+
+print(player.get_weapon())
+player.s_weapon("Sword")
+print(player.get_weapon())
