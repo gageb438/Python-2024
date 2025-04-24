cede9696a698102129703991bc71f55391f419a3
Gage <gageb534@gmail.com>
2025-04-24 09:07:11 -0500

    Update game_classes.py

--- Game/game_classes.py
+++ Game/game_classes.py
@@ -1,10 +1,16 @@
 class Hero():
-    def __init__(self, health, health_max, weapon):
+    def __init__(self, first_name, last_name, health, health_max, weapon):
+        self.__first_name = first_name
+        self.__last_name = last_name
         self.__hp = health
         self.__max_hp = health_max
         self.__weapon = weapon
 
     # getters
+    def get_first_name(self):
+        return self.__first_name
+    def get_last_name(self):
+        return self.__last_name
     def get_hp(self):
         return self.__hp
     
